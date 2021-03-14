4c6ddc1273ef40cc8625f6d846bb0e657ad7445f
Lee Tae Su <taesu@crscube.co.kr>
2021-03-14 22:23:16 +0900

    refactoring runner

--- src/main/kotlin/io/taesu/ktjpajooq/KtjpajooqApplication.kt
+++ src/main/kotlin/io/taesu/ktjpajooq/KtjpajooqApplication.kt
@@ -27,21 +27,25 @@ class Runner(
 ) : ApplicationRunner {
     @Transactional
     override fun run(args: ApplicationArguments?) {
-        // val user1 = userRepository.save(User(
-        //         id = "taesu1",
-        //         name = "Lee1",
-        //         email = "taesu1@crscube.co.kr"))
-        // val user2 = userRepository.save(User(
-        //         id = "taesu2",
-        //         name = "Lee2",
-        //         email = "taesu2@crscube.co.kr"))
-        //
-        // val study = Study(id = "std1", name = "test1 study")
-        // study.addUser(user1)
-        // study.addUser(user1)
-        // study.addUser(user2)
-        // study.addUser(user2)
-        //
-        // studyRepository.save(study)
+        init()
+    }
+
+    private fun init() {
+        val user1 = userRepository.save(User(
+                id = "taesu1",
+                name = "Lee1",
+                email = "taesu1@crscube.co.kr"))
+        val user2 = userRepository.save(User(
+                id = "taesu2",
+                name = "Lee2",
+                email = "taesu2@crscube.co.kr"))
+
+        val study = Study(id = "std1", name = "test1 study")
+        study.addUser(user1)
+        study.addUser(user1)
+        study.addUser(user2)
+        study.addUser(user2)
+
+        studyRepository.save(study)
     }
 }
